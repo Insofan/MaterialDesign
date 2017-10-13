d40885d99566accba1e3339bb7184381d4795ba0
Haixiao <282132315@qq.com>
2017-10-13 21:38:40 +0800

    [mod](AboutMeActivity) 1.完成AboutMeActivity TODO:完成Bottom tab

--- MyMaterialDesign/app/src/main/java/com/example/mymaterialdesign/AboutMeActivity.java
+++ MyMaterialDesign/app/src/main/java/com/example/mymaterialdesign/AboutMeActivity.java
@@ -1,13 +1,17 @@
 package com.example.mymaterialdesign;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by Haixiao on 2017/10/12.
@@ -15,22 +19,30 @@ import android.view.MenuItem;
  * https://github.com/Insofan
  */
 
-public class AboutMeActivity extends AppCompatActivity {
-    private Intent intent;
+public class AboutMeActivity extends AppCompatActivity implements View.OnClickListener {
+    //private value
+    private Button emialButton, githubButton;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_me_activity);
 
-        intent = getIntent();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.about_me_toolbar);
         setSupportActionBar(toolbar);
+
+        emialButton = (Button) findViewById(R.id.about_me_email_button);
+        githubButton = (Button) findViewById(R.id.about_me_github_button);
+
+        emialButton.setOnClickListener(this);
+        githubButton.setOnClickListener(this);
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -53,5 +65,38 @@ public class AboutMeActivity extends AppCompatActivity {
 
         startActivity(backTopIntent);
         super.onBackPressed();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.about_me_email_button:
+                sendEmail(view.getContext());
+                break;
+
+            case R.id.about_me_github_button:
+                 Uri uri1 = Uri.parse("https://github.com/Insofan/MaterialDesign");
+                Intent intent1 = new Intent(Intent.ACTION_VIEW, uri1);
+                startActivity(intent1);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static void sendEmail(Context context) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("plain/text");
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"insofan3156@gmail.com"});
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Material Design Demo");
+        intent.putExtra(Intent.EXTRA_TEXT, "Hi, Insomnia");
+        try {
+            if (!(context instanceof Activity)) {
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            }
+            context.startActivity(intent);
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(context, "Can't find email !", Toast.LENGTH_SHORT).show();
+        }
     }
 }
