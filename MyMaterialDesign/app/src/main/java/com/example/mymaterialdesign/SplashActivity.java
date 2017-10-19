18f6055386ffdedb962626e0c2456512807977c7
Haixiao <282132315@qq.com>
2017-10-19 16:18:57 +0800

    [add](SplashActivity) 1.添加Splash功能

---
+++ MyMaterialDesign/app/src/main/java/com/example/mymaterialdesign/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.mymaterialdesign;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
+
+/**
+ * Created by Haixiao on 2017/10/19.
+ * Email:insofan3156@gmail.com
+ * https://github.com/Insofan
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+    }
+}
